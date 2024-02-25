8192b298be35d899b5da8da8281670a34b6537c0
devkiloton <dev.kiloton@gmail.com>
2024-02-25 15:46:55 -0300

    Creating RegisterRequest

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterRequest;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function register()
+    public function register(RegisterRequest $request)
     {
         return 'hello';
     }
