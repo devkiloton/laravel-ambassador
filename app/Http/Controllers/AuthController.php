33d4ac19b8283cfe33bd572c54a3936dc7549a96
devkiloton <dev.kiloton@gmail.com>
2024-02-25 18:54:29 -0300

    Creating users

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
     public function register(RegisterRequest $request)
     {
-        return 'hello';
+        $user = User::create($request->only('first_name', 'last_name', 'email') + ['password' => \Hash::make($request->input('password')), 'is_admin' => 1]);
+        return response($user, Response::HTTP_CREATED);
     }
 }
