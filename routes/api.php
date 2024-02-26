ea55bcb6c23dd42b60845e0d09387ef9e703de6a
devkiloton <dev.kiloton@gmail.com>
2024-02-26 01:01:00 -0300

    Creating middleware to validate authenticated users

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('admin')->group(function () {
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('user', [AuthController::class, 'user']);
+    });
 });
