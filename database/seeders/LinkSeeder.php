e53d149bd2e48a7759eb00bd76c279be2ec71b6c
devkiloton <dev.kiloton@gmail.com>
2024-02-27 20:38:50 -0300

    Creating Link factory and seeder

---
+++ database/seeders/LinkSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Link;
+use App\Models\LinkProduct;
+use App\Models\Product;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class LinkSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Link::factory(30)->create()->each(function (Link $link) {
+            LinkProduct::create([
+                'link_id' => $link->id,
+                'product_id' => Product::inRandomOrder()->first()->id,
+            ]);
+        });
+    }
+}
