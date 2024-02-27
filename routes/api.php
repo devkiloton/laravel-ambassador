e197cdb59c91c7aacf9154d5991468a5364d9c3a
devkiloton <dev.kiloton@gmail.com>
2024-02-26 22:31:39 -0300

    Creating controller to get ambassadors

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AmbassadorController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -23,5 +24,6 @@ Route::prefix('admin')->group(function () {
         Route::post('logout', [AuthController::class, 'logout']);
         Route::put('users/update', [AuthController::class, 'updateInfo']);
         Route::put('users/update-password', [AuthController::class, 'updatePassword']);
+        Route::get('ambassadors', [AmbassadorController::class, 'index']);
     });
 });
