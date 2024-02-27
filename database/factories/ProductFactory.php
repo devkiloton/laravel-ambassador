0f4c1d9b634dce22c6f806defa3bf54412c66ba1
devkiloton <dev.kiloton@gmail.com>
2024-02-26 23:54:53 -0300

    Creating factory and seeder

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->name,
+            'description' => $this->faker->text,
+            'price' => $this->faker->randomFloat(2, 1, 100),
+            'image' => $this->faker->imageUrl(),
+        ];
+    }
+}
