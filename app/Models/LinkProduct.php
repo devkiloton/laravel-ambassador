327c6dde92650f7ccf9e1982db36b9cd66dd5646
devkiloton <dev.kiloton@gmail.com>
2024-02-27 20:26:16 -0300

    Creating the models

---
+++ app/Models/LinkProduct.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\LinkProduct
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|LinkProduct newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|LinkProduct newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|LinkProduct query()
+ * @mixin \Eloquent
+ */
+class LinkProduct extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+}
