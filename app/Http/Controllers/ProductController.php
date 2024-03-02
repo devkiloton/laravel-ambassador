54fa89578e05e0c04822cf7fbb733dcc4065c60d
devkiloton <dev.kiloton@gmail.com>
2024-03-01 22:54:13 -0300

    Adding cache to ambassador products controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use Cache;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -55,18 +56,36 @@ class ProductController extends Controller
     }
 
     /**
-     * Display a listing of the resource.
+     * Display a listing of the resource to be processed in the frontend.
      */
     public function frontend()
     {
-        return Product::all();
+        if ($products = Cache::get('products_frontend')) {
+            return $products;
+        }
+
+        $products = Product::all();
+        Cache::put('products_frontend', $products, 30); // 30s of TTL
+
+        return $products;
     }
 
     /**
-     * Display a listing of the resource.
+     * Display a paginated listing of the resource processed in the backend.
      */
-    public function backend()
+    public function backend(Request $request)
     {
-        return Product::paginate();
+        $page = $request->input('page', 1);
+        $products = Cache::remember('products_backend', 30, fn () =>  Product::all());
+        $total = $products->count();
+
+        return [
+            'data' => $products->forPage($page, 15)->values(),
+            'meta' => [
+                'total' => $total,
+                'page' => $page,
+                'last_page' => ceil($total / 15)
+            ]
+        ];
     }
 }
