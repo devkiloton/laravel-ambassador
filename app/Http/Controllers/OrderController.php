8622e030a9209a6644a81a4df0ce017ad607a5e6
devkiloton <dev.kiloton@gmail.com>
2024-03-02 13:57:49 -0300

    Preventing partial data creation with transactions

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -7,6 +7,7 @@ use App\Models\Link;
 use App\Models\Order;
 use App\Models\OrderItem;
 use App\Models\Product;
+use DB;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
@@ -24,34 +25,45 @@ class OrderController extends Controller
             abort(400, 'Invalid code');
         }
 
-        $order = new Order();
+        try {
+            DB::beginTransaction();
 
-        $order->code = $link->code;
-        $order->user_id = $link->user->id;
-        $order->ambassador_email = $link->user->email;
-        $order->first_name = $request->input('first_name');
-        $order->last_name = $request->input('last_name');
-        $order->email = $request->input('email');
-        $order->address = $request->input('address');
-        $order->city = $request->input('city');
-        $order->country = $request->input('country');
-        $order->zip = $request->input('zip');
+            $order = new Order();
 
-        $order->save();
+            $order->code = $link->code;
+            $order->user_id = $link->user->id;
+            $order->ambassador_email = $link->user->email;
+            $order->first_name = $request->input('first_name');
+            $order->last_name = $request->input('last_name');
+            $order->email = $request->input('email');
+            $order->address = $request->input('address');
+            $order->city = $request->input('city');
+            $order->country = $request->input('country');
+            $order->zip = $request->input('zip');
 
-        foreach ($request->input('products') as $item) {
-            $product = Product::find($item['product_id']);
+            $order->save();
 
-            $orderItem = new OrderItem();
-            $orderItem->order_id = $order->id;
-            $orderItem->product_title = $product->title;
-            $orderItem->price = $product->price;
-            $orderItem->quantity = $item['quantity'];
-            $orderItem->ambassador_revenue = 0.1 * $product->price * $item['quantity'];
-            $orderItem->admin_revenue = 0.9 * $product->price * $item['quantity'];
+            foreach ($request->input('products') as $item) {
+                $product = Product::find($item['product_id']);
 
-            $orderItem->save();
+                $orderItem = new OrderItem();
+                $orderItem->order_id = $order->id;
+                $orderItem->product_title = $product->title;
+                $orderItem->price = $product->price;
+                $orderItem->quantity = $item['quantity'];
+                $orderItem->ambassador_revenue = 0.1 * $product->price * $item['quantity'];
+                $orderItem->admin_revenue = 0.9 * $product->price * $item['quantity'];
+
+                $orderItem->save();
+            }
+
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            throw $th;
         }
+
+
 
         return $order->load("orderItems");
     }
