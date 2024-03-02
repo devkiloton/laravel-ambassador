c6bee178c7d7c3e80d4e8c8bb4d7bfffba37529e
devkiloton <dev.kiloton@gmail.com>
2024-03-02 12:53:28 -0300

    Adding orders to link retrieving

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\LinkResource;
 use App\Models\Link;
 use App\Models\LinkProduct;
 use Illuminate\Http\Request;
@@ -11,7 +12,8 @@ class LinkController extends Controller
 {
     public function index($id)
     {
-        return Link::where('user_id', $id)->get();
+        $links = Link::with('orders')->where('user_id', $id)->get();
+        return LinkResource::collection($links);
     }
 
     public function store(Request $request)
