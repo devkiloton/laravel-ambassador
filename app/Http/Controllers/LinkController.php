8ae0373fd8d580198ca1b0aba19f4b32d47c4f00
devkiloton <dev.kiloton@gmail.com>
2024-03-02 02:01:14 -0300

    Creating links

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Link;
+use App\Models\LinkProduct;
 use Illuminate\Http\Request;
 
 class LinkController extends Controller
@@ -10,5 +11,23 @@ class LinkController extends Controller
     public function index($id)
     {
         return Link::where('user_id', $id)->get();
+    }
+
+    public function store(Request $request)
+    {
+        $link = Link::create([
+            'user_id' => $request->user()->id,
+            'code' => $request->Str::random(6),
+        ]);
+
+        foreach ($request->input('products') as $product_id) {
+            LinkProduct::create([
+                'link_id' => $link->id,
+                'product_id' => $product_id,
+
+            ]);
+        }
+
+        return response($link, 201);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -51,5 +51,6 @@ Route::prefix('ambassador')->group(function () {
     Route::middleware(['auth:sanctum', 'scope.ambassador'])->group(function () {
         Route::get('stats', [StatsController::class, 'index']);
         Route::get('rankings', [StatsController::class, 'rankings']);
+        Route::post('links', [LinkController::class, 'store']);
     });
 });
