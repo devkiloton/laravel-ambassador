0c925e4db0c12cbec3546e53f78aa9eafc9a0814
devkiloton <dev.kiloton@gmail.com>
2024-03-05 22:33:48 -0300

    Seeding the whole with one command

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,9 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             AmbassadorSeeder::class,
+            ProductSeeder::class,
+            LinkSeeder::class,
+            OrderSeeder::class,
         ]);
     }
 }
